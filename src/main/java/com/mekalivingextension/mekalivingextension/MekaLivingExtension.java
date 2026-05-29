bc69a23f3553a3ebd43f31a77792b32b120bb824
Richie-liu111 <18851848776@163.com>
2026-05-29 22:06:06 +0800

    5.29

---
+++ src/main/java/com/mekalivingextension/mekalivingextension/MekaLivingExtension.java
@@ -0,0 +1,9 @@
+package com.mekalivingextension.mekalivingextension;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod("meka_living_extension")
+public class MekaLivingExtension {
+    public MekaLivingExtension() {
+    }
+}
